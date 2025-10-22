ee6bf275239aca1b1c03837302b156af49a20dfd
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-22 13:54:37 +0200

    ex 2 finish

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,20 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+//Soluzione con il ciclo For
+
+for (let index = 0; index < people.length; index++) {
+  const person = people[index];
+  console.log(person.name);
+}
+
+//Soluzione con il ciclo ForEach
+people.forEach((person) => {
+  console.log(person.name);
+});
