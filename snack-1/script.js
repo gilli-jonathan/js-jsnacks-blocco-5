9508605c550f76efa482943b1232fc5742ddee28
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-22 13:42:14 +0200

    snack 1 solustion for

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,12 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//Soluzione con il classico ciclo For
+for (let index = 0; index < names.length; index++) {
+  const name = names[index];
+  console.log(name);
+}
+
+//Soluzione con il classico ciclo For
