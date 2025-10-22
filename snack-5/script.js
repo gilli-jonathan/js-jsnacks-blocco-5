dbe7fef6de3dbdddbfea95872848ab6574679aaf
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-22 14:15:09 +0200

    snack 5 ciclo for

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,15 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+//soluzione con il ciclo For
+const nums_pari = [];
+
+for (let index = 0; index < nums.length; index++) {
+  const nu = nums[index];
+  if (nu % 2 === 0) {
+    nums_pari.push(nu);
+  }
+}
+
+console.log(nums_pari);
