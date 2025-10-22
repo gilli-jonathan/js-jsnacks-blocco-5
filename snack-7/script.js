e2104dd986265743667c615bcb750965d51a45cc
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-22 15:04:26 +0200

    snack 7 with cicle for

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,23 +1,35 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//soluzione con il ciclo for
+
+let obj = {};
+
+for (let index = 0; index < students.length; index++) {
+  const tipino = students[index];
+  if (tipino.id === 2) {
+    obj = tipino;
+  }
+}
+console.log(obj);
